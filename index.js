72d7ecc794c0a8e3fcba55c8654d2d28609f4f92
desmond <desmond@desmonds-MacBook-Pro.local>
2022-04-12 14:38:54 +0300

    task complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+
+   
+
+const superbowlWin = (record) => { [
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2015", result: "W"},
+   ]
+    const season = record.find((season) => season.result === 'W');
+    if (season != undefined) {
+      return season.year;
+    } else {
+      return season;
+    }
+  };
